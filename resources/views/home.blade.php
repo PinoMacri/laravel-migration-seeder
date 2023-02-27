f3899d140f1679f3f82fd9ca0462ec3cabfc9952
PinoMacri <pinonegozio1@outlook.it>
2023-02-27 19:34:01 +0100

    Finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,15 @@
         @foreach ($trains as $train)
             <li>
                 <ul>
-                    <li>{{ $train->azienda }}</li>
-                    <li>{{ $train->stazione_partenza }}</li>
-                    <li>{{ $train->stazione_arrivo }}</li>
-                    <li>{{ $train->orario_partenza }}</li>
-                    <li>{{ $train->orario_arrivo }}</li>
-                    <li>{{ $train->codice_treno }}</li>
-                    <li>{{ $train->numero_carrozze }}</li>
-                    <li>{{ $train->in_orario }}</li>
-                    <li>{{ $train->cancellato }}</li>
+                    <li>Azienda Treno:{{ $train->azienda }}</li>
+                    <li>Stazione di Partenza:{{ $train->stazione_partenza }}</li>
+                    <li>Stazione di Arrivo:{{ $train->stazione_arrivo }}</li>
+                    <li>Orario di Partenza:{{ $train->orario_partenza }}</li>
+                    <li>Orario di Arrivo:{{ $train->orario_arrivo }}</li>
+                    <li>Codice Treno:{{ $train->codice_treno }}</li>
+                    <li>Numero Carrozze:{{ $train->numero_carrozze }}</li>
+                    <li>Treno in orario:{{ $train->in_orario }}</li>
+                    <li>Treno cancellato:{{ $train->cancellato }}</li>
                 </ul>
             </li>
         @endforeach
